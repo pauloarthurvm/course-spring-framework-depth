b488dcda5e8c065b5a720d1df80a31f706105898
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-01-31 19:48:24 -0300

    Implement AOP advice: Around

--- src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
+++ src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
@@ -1,10 +1,8 @@
 package com.frankmoley.lil.fid.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -22,10 +20,14 @@ public class LoggingAspect {
     @Pointcut("@annotation(Loggable)")
     public void executeLogging() {}
 
-    @AfterReturning(value = "executeLogging()", returning = "returnValue")
-    public void logMethodCall(JoinPoint joinPoint, Object returnValue) {
+    @Around(value = "executeLogging()")
+    public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object returnValue = joinPoint.proceed();
+        long totalTime = System.currentTimeMillis() - startTime;
         StringBuilder message = new StringBuilder("Method: ");
         message.append(joinPoint.getSignature().getName());
+        message.append(" totalTime: ").append(totalTime).append("ms");
         Object[] args = joinPoint.getArgs();
         if (null != args && args.length > 0) {
             message.append(" args = [ | ");
@@ -41,6 +43,7 @@ public class LoggingAspect {
             }
         }
         LOGGER.info(message.toString());
+        return returnValue;
     }
 
 }
