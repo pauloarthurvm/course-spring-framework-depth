e0e2535ac3095444112725a48300804565a5b705
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-01-31 19:41:25 -0300

    Implement AOP advice: After

--- src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
+++ src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.frankmoley.lil.fid.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
+import java.util.Collection;
 
 @Component
 @Aspect
@@ -20,8 +22,8 @@ public class LoggingAspect {
     @Pointcut("@annotation(Loggable)")
     public void executeLogging() {}
 
-    @Before("executeLogging()")
-    public void logMethodCall(JoinPoint joinPoint) {
+    @AfterReturning(value = "executeLogging()", returning = "returnValue")
+    public void logMethodCall(JoinPoint joinPoint, Object returnValue) {
         StringBuilder message = new StringBuilder("Method: ");
         message.append(joinPoint.getSignature().getName());
         Object[] args = joinPoint.getArgs();
@@ -31,6 +33,12 @@ public class LoggingAspect {
                 message.append(arg).append(" | ");
             });
             message.append("]");
+
+            if(returnValue instanceof Collection) {
+                message.append(", returning: ").append(((Collection)returnValue).size()).append(" instance(s)");
+            } else {
+                message.append(", returning: ").append(returnValue.toString());
+            }
         }
         LOGGER.info(message.toString());
     }
